8229f767d837d916f0a780953eee85a6b5867563
Ny'alotha <Nyalotha@outlook.com>
2017-09-14 16:12:14 +0300

    customers are now loaded from the database.

--- Movly/Controllers/CustomersController.cs
+++ Movly/Controllers/CustomersController.cs
@@ -10,32 +10,46 @@ namespace Movly.Controllers
 {
     public class CustomersController : Controller
     {
-        private List<Customer> customers;
+        //private List<Customer> customers;
+        private ApplicationDbContext _context;
+
         public CustomersController()
         {
-            customers = new List<Customer>
-            {
-                new Customer { Name = "John Smith"},
-                new Customer { Name = "Amy White"}
-            };
+            _context = new ApplicationDbContext();
         }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //    {
+        //        new Customer { Id =1, Name = "John Smith"},
+        //        new Customer { Id =2, Name = "Amy White"}
+        //    };
+        //}
 
         // GET: Customers
         public ActionResult Index()
         {
-            var viewModel = new CustomersViewModel
-            {
-                Customers = customers
-            };
+            var customers = _context.Customers;
+            //var viewModel = new CustomersViewModel
+            //{
+            //    Customers = customers
+            //};
 
-            return View(viewModel);
+            return View(customers);
         }
 
         [Route ("Customers/Details/{id}")]
         public ActionResult Details(int id)
         {
-            if (id != 0 && id <= customers.Count())
-                return View(customers[--id]);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer != null)
+                return View(customer);
             else
                 return HttpNotFound();
         }
