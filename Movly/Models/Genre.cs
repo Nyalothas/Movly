35a37f11968bd2a47b65aebfdfa00e010d89b511
Ny'alotha <Nyalotha@outlook.com>
2017-09-15 09:44:27 +0300

    added Genre and Movie props/ Populated Genre

--- Movly/Models/Genre.cs
+++ Movly/Models/Genre.cs
@@ -6,20 +6,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Movly.Models
 {
-    public class Movie
+    public class Genre
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-
-        [Required]
-        public Genre Genre { get; set; }
-        public byte GenreId { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public DateTime DateAdded { get; set; }
-        public byte NumberInStoc { get; set; }
-
-
     }
 }
