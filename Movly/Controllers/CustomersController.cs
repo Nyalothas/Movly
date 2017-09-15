109a3564418fba1f3c40ca00ab770ad209ffd711
Ny'alotha <Nyalotha@outlook.com>
2017-09-15 15:38:49 +0300

    added ability to edit a Customer and save it in the database

--- Movly/Controllers/CustomersController.cs
+++ Movly/Controllers/CustomersController.cs
@@ -43,21 +43,58 @@ namespace Movly.Controllers
         {
             var membershipTypes = _context.MembershipTypes.ToList();
 
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
+        }
+
+        [HttpPost] //if access modify data, should not be accessibl e by httpGet
+        public ActionResult Save(Customer customer) //binds this model to the request data
+        {
+            if(customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                //TryUpdateModel(customerInDb); kinda bad
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipType = customer.MembershipType;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
 
         [Route ("Customers/Details/{id}")]
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c =>c.MembershipType).SingleOrDefault(c => c.Id == id);
-            if (customer != null)
-                return View(customer);
-            else
+            if (customer == null)
                 return HttpNotFound();
+
+            return View(customer);
+                
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
