c4bac021f9e34dc53eeaf1cc9c55a2cf0c336b14
Ny'alotha <Nyalotha@outlook.com>
2017-09-19 11:54:04 +0300

    added Movies API

--- Movly/App_Start/MappingProfile.cs
+++ Movly/App_Start/MappingProfile.cs
@@ -12,9 +12,12 @@ namespace Movly.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            //ForMemeber to ignore the key that should not be modified
+            Mapper.CreateMap<Customer, CustomerDto>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
 
+            Mapper.CreateMap<Movie, MovieDto>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
